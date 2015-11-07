f9c65995105bb302d494d5db37a6be664842a7ec
Markus Keppler <markus.keppler@udo.edu>
2015-11-07 12:16:40 +0100

    tabellen

--- Protokolle/Biegung_elastischer_Stäbe/Auswertung/differenz.py
+++ Protokolle/Biegung_elastischer_Stäbe/Auswertung/differenz.py
@@ -10,11 +10,15 @@ w1,w2,ortr=np.genfromtxt('messungrechteck.txt',unpack=True)
 ortr*=0.001
 rdiff=(w1-w2)*0.01/1000
 print(rdiff)
+np.savetxt('diffrecht.txt', np.column_stack([rdiff*1000]), header='diff in m')
+
 
 q1,q2,ortz=np.genfromtxt('messungzylinder.txt',unpack=True)
 ortz*=0.001
 zdiff=(q1-q2)*0.01/1000
 print(zdiff)
+np.savetxt('diffzyl.txt', np.column_stack([zdiff*1000]), header='diff in m')
+
 
 e1,e2,ortzweis=np.genfromtxt('rechteckzweiseitig.txt',unpack=True)
 a=range(0,25)
@@ -24,15 +28,20 @@ uhr2ortzweis=np.array(ortzweis[b])
 
 uhr1ortzweis*=0.001
 uhr2ortzweis*=0.001
-print(uhr1ortzweis)
-print(uhr2ortzweis)
 
 zweidiff1=(e1[a]-e2[a])*0.01/1000
 zweidiff2=(e1[b]-e2[b])*0.01/1000
-print(zweidiff2)
+
+np.savetxt('diffzwei1.txt', np.column_stack([zweidiff1]), header='diff in m')
+np.savetxt('diffzwei2.txt', np.column_stack([zweidiff2]), header='diff in m')
 
 def g(x,L):
     return L*x**2-x**3/3
+np.savetxt('diffrechtg(x).txt', np.column_stack([g(ortr,LeingespR)]), header='g(x)in m^3')
+np.savetxt('diffzylg(x).txt', np.column_stack([g(ortz,LeingespZ)]), header='g(x)')
+np.savetxt('diffzwei1g(x).txt', np.column_stack([g(uhr1ortzweis,LzweieingespR)]), header='g(x)')
+np.savetxt('diffzwei2g(x).txt', np.column_stack([g(uhr2ortzweis,LzweieingespR)]), header='g(x)')
+
 
 W1,W1err,W2,W2err=linregress(g(ortr,LeingespR),rdiff)
 Q1,Q1err,Q2,Q2err=linregress(g(ortz,LeingespZ),zdiff)
