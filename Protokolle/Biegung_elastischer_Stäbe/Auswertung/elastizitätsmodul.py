5b77b9bb94c0443fb2f6f66e213d69c41e5b36f2
Markus Keppler <markus.keppler@udo.edu>
2015-11-07 10:14:47 +0100

    alles in den ordner Auswertung

--- Protokolle/Biegung_elastischer_Stäbe/Auswertung/elastizitätsmodul.py
+++ Protokolle/Biegung_elastischer_Stäbe/Auswertung/elastizitätsmodul.py
@@ -3,12 +3,18 @@ import numpy as np
 from uncertainties import ufloat
 from differenz import W1,W2,Q1,Q2,E1,E2,E1_,E2_
 from kraft import F1,F2
+from dichte import r
+import math
 
 # E=F/(2*I*msteig)
 b=ufloat(np.mean([.012,.012,.012,.012,.012,.012,.012,.012,.012,.0198]),np.std([.012,.012,.012,.012,.012,.012,.012,.012,.012,.0198]))
 h=.01
-I=1/12*b*h**3
-print(I)
+Ir=1/12*b*h**3
 
-Erecht=F1/(2*W1*I)
-print(Erecht)
+Iz=np.pi/4*r**4
+
+Erecht=F1/(2*W1*Ir)
+Ezyl=F1/(2*Q1*Iz)
+
+print('Erecht',Erecht)
+print('Ezyl',Ezyl)
