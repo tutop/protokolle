c43bb4e2c695d6020201c2c8e0e1c2743e76138d
Markus Keppler <markus.keppler@udo.edu>
2015-11-08 20:19:44 +0100

    fehler behoben

--- Protokolle/Biegung_elastischer_Stäbe/Auswertung/differenz.py
+++ Protokolle/Biegung_elastischer_Stäbe/Auswertung/differenz.py
@@ -39,26 +39,26 @@ def g(x,L):
     return L*x**2-x**3/3
 np.savetxt('diffrechtg(x).txt', np.column_stack([g(ortr,LeingespR)]), header='g(x)in m^3')
 np.savetxt('diffzylg(x).txt', np.column_stack([g(ortz,LeingespZ)]), header='g(x)')
-np.savetxt('diffzwei1g(x).txt', np.column_stack([g(uhr1ortzweis,LzweieingespR)]), header='g(x)')
-np.savetxt('diffzwei2g(x).txt', np.column_stack([g(uhr2ortzweis,LzweieingespR)]), header='g(x)')
+# np.savetxt('diffzwei1g(x).txt', np.column_stack([g(uhr1ortzweis,LzweieingespR)]), header='g(x)')
+# np.savetxt('diffzwei2g(x).txt', np.column_stack([g(uhr2ortzweis,LzweieingespR)]), header='g(x)')
 
 def f(x,L):
     return 3*L**2*x-4*x**3
 # np.savetxt('diffrechtf(x).txt', np.column_stack([f(ortr,LeingespR)]), header='g(x)in m^3')
 # np.savetxt('diffzylf(x).txt', np.column_stack([f(ortz,LeingespZ)]), header='g(x)')
-np.savetxt('diffzwei1f(x).txt', np.column_stack([f(uhr1ortzweis,LzweieingespR)]), header='g(x)')
+np.savetxt('diffzwei2f(x).txt', np.column_stack([f(uhr2ortzweis,LzweieingespR)]), header='g(x)')
 # np.savetxt('diffzwei2f(x).txt', np.column_stack([f(uhr2ortzweis,LzweieingespR/2)]), header='g(x)')
 
 def h(x,L):
     return 4*x**3-12*L*x**2+9*L**2*x-L**3
-np.savetxt('diffzwei2h(x).txt', np.column_stack([f(uhr2ortzweis,LzweieingespR)]), header='g(x)')
+np.savetxt('diffzwei1f(x).txt', np.column_stack([h(uhr1ortzweis,LzweieingespR)]), header='g(x)')
 
 
 
 # fW1,fW1err,fW2,fW2err=linregress(f(ortr,LeingespR),rdiff)
 # fQ1,fQ1err,fQ2,fQ2err=linregress(f(ortz,LeingespZ),zdiff)
-fE1,fE1err,fE2,fE2err=linregress(f(uhr1ortzweis,LzweieingespR),zweidiff1)
-hE1_,hE1err_,hE2_,hE2err_=linregress(h(uhr2ortzweis,LzweieingespR),zweidiff2)
+hE1,hE1err,hE2,hE2err=linregress(h(uhr1ortzweis,LzweieingespR),zweidiff1)
+fE1_,fE1err_,fE2_,fE2err_=linregress(f(uhr2ortzweis,LzweieingespR),zweidiff2)
 
 
 W1,W1err,W2,W2err=linregress(g(ortr,LeingespR),rdiff)
@@ -68,8 +68,8 @@ Q1,Q1err,Q2,Q2err=linregress(g(ortz,LeingespZ),zdiff)
 
 print('R=',W1)
 print('Z=',Q1)
-print('fE1=',fE1)
-print('hE1=',hE1_)
+print('weiterweg hE1=',hE1)
+print('näheran null fE1=',fE1_)
 
 
 
@@ -98,7 +98,7 @@ plt.savefig('plot2.pdf')
 plt.close()
 
 plt.plot(f(uhr1ortzweis,LzweieingespR),zweidiff1,'gx',label='zweiseitig eingespanntes Rechteck')
-plt.plot(x,fE2+x*fE1,'g-')
+plt.plot(x,hE2+x*hE1,'g-')
 # plt.xlim(0,0.1)
 plt.xlabel(r'$g(x) \ / \ m^3$')
 plt.ylabel(r'$D \ / \ m$')
@@ -108,7 +108,7 @@ plt.savefig('plot3.pdf')
 plt.close()
 
 plt.plot(h(uhr2ortzweis,LzweieingespR),zweidiff2,'gx',label='zweiseitig eingespanntes Rechteck')
-plt.plot(x,hE2_+x*hE1_,'g-')
+plt.plot(x,fE2_+x*fE1_,'g-')
 # plt.xlim(0,0.1)
 plt.xlabel(r'$g(x) \ / \ m^3$')
 plt.ylabel(r'$D \ / \ m$')
--- Protokolle/Biegung_elastischer_Stäbe/Auswertung/elastizitätsmodul.py
+++ Protokolle/Biegung_elastischer_Stäbe/Auswertung/elastizitätsmodul.py
@@ -1,7 +1,7 @@
 #(8) einseitig eingespannt
 import numpy as np
 from uncertainties import ufloat
-from differenz import W1,W2,Q1,Q2,E1,E2,E1_,E2_
+from differenz import W1,W2,Q1,Q2,hE1,hE2,fE1_,fE2_
 from kraft import F1,F2
 from dichte import r
 import math
@@ -15,8 +15,8 @@ Iz=np.pi/4*r**4
 
 Erecht=F1/(2*W1*Ir)
 Ezyl=F1/(2*Q1*Iz)
-Ezweilinks=F2/(48*Ir*E1)
-Ezweirechts=F2/(48*Ir*E1_)
+Ezweilinks=F2/(48*Ir*hE1)
+Ezweirechts=F2/(48*Ir*fE1_)
 
 print('Ir=',Ir)
 print('Iz=',Iz)
