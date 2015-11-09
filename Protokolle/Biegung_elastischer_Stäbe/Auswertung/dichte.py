f2a9ca6a3e419167c141e72a219262eb5115d3fb
Markus Keppler <markus.keppler@udo.edu>
2015-11-09 16:56:15 +0100

    Fertig nur noch formel verweise

--- Protokolle/Biegung_elastischer_Stäbe/Auswertung/dichte.py
+++ Protokolle/Biegung_elastischer_Stäbe/Auswertung/dichte.py
@@ -4,9 +4,9 @@ import math
 #rechteck Stab
 
 l=0.602
-m=605.1
+m=0.6051
 h=0.01
-rvolumen=ufloat((np.mean([.012,.012,.012,.012,.012,.012,.012,.012,.012,.0198])*l*h,np.std([.012,.012,.012,.012,.012,.012,.012,.012,.012,.0198])*l*h))
+rvolumen=ufloat((np.mean([.012,.012,.012,.012,.012,.012,.012,.012,.012,.01198])*l*h,np.std([.012,.012,.012,.012,.012,.012,.012,.012,.012,.01198])*l*h))
 rdichte=m/rvolumen
 
 print('Rechteckvolumen =',rvolumen)
@@ -15,10 +15,10 @@ print('b=',np.mean([.012,.012,.012,.012,.012,.012,.012,.012,.012,.0198]),np.std(
 #zylinder Stab.2
 
 l=0.58
-m=356.4
-r=ufloat(np.mean([.01,.01,.01,.01,.01,.01,.01,0.098,0.098,0.098,])*0.5,np.std([.01,.01,.01,.01,.01,.01,.01,0.098,0.098,0.098,])*0.5)
-zylvolumen=(np.pi*r*l)
+m=0.3564
+r=ufloat(np.mean([.01,.01,.01,.01,.01,.01,.01,0.0098,0.0098,0.0098,])*0.5,np.std([.01,.01,.01,.01,.01,.01,.01,0.0098,0.0098,0.0098,])*0.5)
+zylvolumen=(np.pi*r**2*l)
 zyldichte=m/zylvolumen
 
 print('zylinderdichte=',zyldichte)
-print('d=',r*0.5)
+print('d=',r*2)
